2d86cde8b5c8350c25ea8f1153d52563f998b0bc
scottsilver <scottsilver47@gmail.com>
2020-05-19 10:45:13 -0500

    added checkSlide and debounce functions and hooked up eventListener for 'scroll'

--- JavaScript30/13 - Slide in on Scroll/index.js
+++ JavaScript30/13 - Slide in on Scroll/index.js
@@ -18,7 +18,21 @@ function debounce(func, wait = 20, immediate = true) {
   const sliderImages = document.querySelectorAll('.slide-in')
 
   function checkSlide (e) {
-      console.log(e)
+      //loop over images and mark when they will be fully shown on page
+      sliderImages.forEach(sliderImage => {
+          //halfway through the image
+          const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+          //bottom image
+          const imageBottom = sliderImage.offsetTop + sliderImage.height;
+          const isHalfShown = slideInAt > sliderImage.offSetTop;
+          const isNotScrollPast = window.scrollY < imageBottom;
+          if (isHalfShown && !isNotScrollPast) {
+              sliderImage.classList.add('active')
+          } else {
+              sliderImage.classList.remove('active');
+          }
+          console.log(slideInAt)
+      })
   }
 
   window.addEventListener('scroll', debounce(checkSlide))
